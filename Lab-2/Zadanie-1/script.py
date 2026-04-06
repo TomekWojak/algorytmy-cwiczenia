9539079be37191b84244be7e98c06b72678952db
Tomek <tomek.wojak796@gmail.com>
2026-04-06 21:37:18 +0200

    finished task 1

---
+++ Lab-2/Zadanie-1/script.py
@@ -0,0 +1,11 @@
+# for i in range(n):
+#  for j in range(n // 2):
+#     print(i)
+
+# pętla zewnętrzna wykonuje się n razy (liczba iteracji n)
+# pętla wewnętrzna wykonuje się n/2 razy (liczba iteracji n/2)
+
+# n * n / 2 = n^2 / 2
+
+# w notacji Big-O ignorujemy stałe - w tym wypadku 1/2
+# otrzymujemy O(n^2)
