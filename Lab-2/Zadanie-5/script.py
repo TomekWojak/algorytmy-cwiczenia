bdff56de38c750d20bf5007e43e31b0af6759d29
Tomek <tomek.wojak796@gmail.com>
2026-04-08 15:18:20 +0200

    finished task 5

--- Lab-2/Zadanie-5/script.py
+++ Lab-2/Zadanie-5/script.py
@@ -12,7 +12,7 @@ def binary_search(arr, left, right, target):
         return binary_search(arr, mid + 1, right, target)
 
 
-arr = [2, 5, 8, 12, 15, 18, 22, 27, 30, 35, 40, 45, 50, 55, 60]
+arr = [2, 5, 8, 12, 15, 18, 22, 27, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85]
 target = 55
 
 result = binary_search(arr, 0, len(arr) - 1, target)
