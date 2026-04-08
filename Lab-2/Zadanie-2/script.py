8891e738116024e7b9e221f65b00aaffe0677bee
Tomek <tomek.wojak796@gmail.com>
2026-04-08 11:35:03 +0200

    finished task 2

---
+++ Lab-2/Zadanie-2/script.py
@@ -0,0 +1,11 @@
+# sum = 0
+# for i in range(n):
+#  for j in range(2 * n):
+#     sum += i + j
+
+# pętla zewnętrzna wykona się n razy
+# pętla wewnętrzna wykona się 2n razy
+# n * 2n = 2n^2 <= liczba iteracji pętli
+
+# w notacji Big-O ignorujemy stałe - w tym wypadku 2
+# otrzymujemy O(n^2)
