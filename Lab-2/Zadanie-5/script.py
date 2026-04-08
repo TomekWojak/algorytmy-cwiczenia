25cec9bb551620d96575bc07e1e2ce0be1a20703
Tomek <tomek.wojak796@gmail.com>
2026-04-08 15:17:09 +0200

    finished task 5

---
+++ Lab-2/Zadanie-5/script.py
@@ -0,0 +1,23 @@
+def binary_search(arr, left, right, target):
+    if left > right:
+        return -1
+
+    mid = (left + right) // 2
+
+    if arr[mid] == target:
+        return mid
+    elif arr[mid] > target:
+        return binary_search(arr, left, mid - 1, target)
+    else:
+        return binary_search(arr, mid + 1, right, target)
+
+
+arr = [2, 5, 8, 12, 15, 18, 22, 27, 30, 35, 40, 45, 50, 55, 60]
+target = 55
+
+result = binary_search(arr, 0, len(arr) - 1, target)
+
+if result != -1:
+    print("Znaleziono. Indeks elementu:", result)
+else:
+    print("Nie znaleziono")
