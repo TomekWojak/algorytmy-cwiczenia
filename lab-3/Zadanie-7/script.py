75754e3bbd12322652eece81b8685667e4a27f4b
Tomek <tomek.wojak796@gmail.com>
2026-04-26 22:58:52 +0200

    Completed tasks 7

---
+++ lab-3/Zadanie-7/script.py
@@ -0,0 +1,42 @@
+def uruchom_przychodnie():
+    kolejka = []
+    
+    menu = '''\nWybierz działanie:
+1. Zarejestruj pacjenta
+2. Wywołaj pacjenta do gabinetu
+3. Pokaż aktualną kolejkę
+4. Zakończ działanie programu'''
+
+    while True:
+        print(menu)
+        wybor = input("Twój wybór: ")
+
+        if wybor == '1':
+            nazwisko = input("Podaj nazwisko pacjenta: ")
+            kolejka.append(nazwisko)
+            print(f"Pacjent {nazwisko} został zarejestrowany.")
+        
+        elif wybor == '2':
+            if len(kolejka) > 0:
+                pacjent = kolejka.pop(0)
+                print(f"Pacjent {pacjent} proszony do gabinetu.")
+            else:
+                print("Błąd: Kolejka jest pusta!")
+        
+        elif wybor == '3':
+            if not kolejka:
+                print("Kolejka jest aktualnie pusta.")
+            else:
+                print("Aktualna kolejka pacjentów:")
+                for i, pacjent in enumerate(kolejka, 1):
+                    print(f"{i}. {pacjent}")
+        
+        elif wybor == '4':
+            print("Zamykanie systemu")
+            break
+        
+        else:
+            print("Nieprawidłowy wybór, spróbuj ponownie.")
+
+if __name__ == "__main__":
+    uruchom_przychodnie()
